277f6dd69553a9b8aa3891899f15f2a84d6fc30b
Ilgova <ilgova.1594@mail.ru>
2023-02-17 01:18:11 +0300

    first commit

---
+++ 066/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void IntervalSum (int M, int N, int sum)
+{
+    if (M > N) 
+    {
+        Console.Write($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (M++);
+    IntervalSum(M, N, sum);
+}
+
+IntervalSum(M, N, 0);
---
+++ 068/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Console.Write("Введите число m: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число n: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (m < 0 || n < 0) Console.WriteLine("Введено отрицательое число!");
+    int result = FunctionAckermann(m, n);
+    Console.WriteLine($"Результат вычисления функции Аккермана c помощью рекурсии A({m},{n}) = {result}.");
+
+
+int FunctionAckermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m != 0 && n == 0) return FunctionAckermann(m - 1, 1);
+    if (m > 0 && n > 0) return FunctionAckermann(m - 1, FunctionAckermann(m, n - 1));
+    return FunctionAckermann(m, n);
+}
